09db54acd325a19f9a7e5ece69cf753077d5d325
Thanh Hương <116507288+nguyenthithanhhuong@users.noreply.github.com>
2023-03-12 20:45:57 +0700

    Update

---
+++ assets/javascript/ModalMenu8.js
@@ -0,0 +1,90 @@
+// Modal duck
+var modalDuck = document.getElementById('duck');
+        
+var btnDuck = document.getElementById('i__duck');
+
+btnDuck.onclick = function() {
+    duck.style.display = "flex"
+}
+
+modalDuck.onclick = function() {
+    duck.style.display = "none"
+}
+
+// Modal dog
+var modalDog = document.getElementById('dog');
+        
+var btnDog = document.getElementById('i__dog');
+
+btnDog.onclick = function() {
+    dog.style.display = "flex"
+}
+
+modalDog.onclick = function() {
+    dog.style.display = "none"
+}
+
+// Modal whale
+var modalWhale = document.getElementById('whale');
+        
+var btnWhale = document.getElementById('i__whale');
+
+btnWhale.onclick = function() {
+    whale.style.display = "flex"
+}
+
+modalWhale.onclick = function() {
+    whale.style.display = "none"
+}
+
+// Modal penguin
+var modalPenguin = document.getElementById('penguin');
+        
+var btnPenguin = document.getElementById('i__penguin');
+
+btnPenguin.onclick = function() {
+    penguin.style.display = "flex"
+}
+
+modalPenguin.onclick = function() {
+    penguin.style.display = "none"
+}
+
+// Modal frogFat
+var modalFrogFat = document.getElementById('frogFat');
+        
+var btnFrogFat = document.getElementById('i__frogFat');
+
+btnFrogFat.onclick = function() {
+    frogFat.style.display = "flex"
+}
+
+modalFrogFat.onclick = function() {
+    frogFat.style.display = "none"
+}
+
+// Modal frogStress
+var modalFrogStress = document.getElementById('frogStress');
+        
+var btnFrogStress = document.getElementById('i__frogStress');
+
+btnFrogStress.onclick = function() {
+    frogStress.style.display = "flex"
+}
+
+modalFrogStress.onclick = function() {
+    frogStress.style.display = "none"
+}
+
+// Modal warmCream
+var modalWarmCream = document.getElementById('warmCream');
+        
+var btnWarmCream = document.getElementById('i__warmCream');
+
+btnWarmCream.onclick = function() {
+    warmCream.style.display = "flex"
+}
+
+modalWarmCream.onclick = function() {
+    warmCream.style.display = "none"
+}
